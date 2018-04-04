13d7ae4c29d56b394d16cc9aa3788d68dfd70bd1
Amy <arashi@tutanota.de>
2018-04-04 10:03:46 +0200

    added main pages

---
+++ components/about/companyStory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h2 class="is-size-1">
+      Amy Storm Kosman creative
+    </h2> 
+    <p>
+      In 2016 I began to think I was ready to start a small business again. I thought of my passions and began to explore fashion accessories. I experimented with many DIY tutorials and books.
+      I also read up on the environmental impact of the fashion industry. Environmental issues have always been an interest of mine. It seems to me that the huge mess we make 
+      on this Earth will have to be cleaned up eventually by someone. I try not to add to that eventual burden. I also think there are many gorgeous vintage fabrics out there.
+    </p>
+    <br>
+    <p>
+      I decided my business would strive to diminish the amount of wasted fabric by using the environmental 3 R's: reduce, reuse, and renew, as well as embracing the Japanese culture of mottainai. 
+      By artfully restoring the beauty of vintage fabrics, and discarded textiles into fashionable accessories I can inspire a wardrobe with creativity and personality. 
+      Creating handmade accessories is a practical application of the sustainable fashion movement. It's also fun to make (and shop for). 
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'companyStory'
+}
+</script>
+
+<style scoped>
+.container {
+  margin-bottom: 8vh;
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <section class="pageStuff">
+    <contact-hero/>
+    <contact-part/>
+    <quick-message/>
+    <signup-newsletter/>
+  </section>
+</template>
+
+<script>
+import contactHero from '~/components/contact/contactHero.vue'
+import contactPart from '~/components/contact/contactPart.vue'
+import quickMessage from '~/components/quickMessage.vue'
+import signupNewsletter from '~/components/signupNewsletter.vue'
+
+export default {
+  layout: 'defaultLayout',
+  components: {
+    contactHero,
+    contactPart,
+    quickMessage,
+    signupNewsletter
+  },
+  head () {
+    return {
+      title: 'Amy Storm Kosman creative - Contact',
+      meta: [
+        { hid: 'description', name: 'description', content: 'All the information needed to contact Amy and Amy Storm Kosman creative.' }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
