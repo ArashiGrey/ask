fd2d1750c9465cdbb13a6218c031f44c5457887c
Amy <arashi@tutanota.de>
2018-04-04 22:39:56 +0200

    netlifycms setup

---
+++ pages/admin/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <div>
+      <h2>
+        Admin
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+import CMS from 'netlify-cms'
+
+export default {
+  head: {
+    script: [
+      { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+    ]
+  }
+}
+</script>
+
+<style>
+</style>
