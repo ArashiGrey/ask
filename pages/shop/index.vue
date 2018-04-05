5fa7899fe59579e3515e544efcd34e801d37de63
Amy <arashi@tutanota.de>
2018-04-05 21:35:41 +0200

    add shop pages

---
+++ pages/shop/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="pageStuff">
+    <shop-hero/>
+    <shop-Main/>
+  </section>
+</template>
+
+<script>
+import shopHero from '~/components/shop/shopHero.vue'
+import shopMain from '~/components/shop/shopMain.vue'
+
+export default {
+  layout: 'others',
+  components: {
+    shopHero,
+    shopMain
+  },
+  scrollToTop: true,
+  head () {
+    return {
+      title: 'Amy Storm Kosman creative - Shop',
+      meta: [
+        { hid: 'description', name: 'description', content: 'A place to buy all things Amy Storm Kosman creative.' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
