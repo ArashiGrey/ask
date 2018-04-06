7d87b7de844bc1283ef92a03e33d98d50858b1aa
Amy <arashi@tutanota.de>
2018-04-06 10:55:16 +0200

    newletter

--- components/signupNewsletter.vue
+++ components/signupNewsletter.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="newsletter">
-    <form name="contact" method="POST" netlify>
     <div class="columns is-mobile is-centered">
       <div class="column is-three-fifths">
         <div class="card">
@@ -14,26 +13,49 @@
           </div>
           <div class="card-content">
             <div class="content">
-              <div class="field">
-                <label for="name" class="label">Title</label>
-                <input class="input" type="text" name="title" placeholder="Title" v-model="form.title" />
-              </div>
-              <div class="field">
-                <label for="email" class="label">Email</label>
-                <input class="input" type="email" name="email" placeholder="Email" v-model="form.email" />
-              </div>
-            </div>
-            <div class="field is-grouped">
-              <button type="button" v-if="status === 1">Please wait.</button>
-              <button class="button is-medium is-pink" type="button" v-else @click="send">Submit</button>
+              <form name="contact" method="POST" netlify>
+
+                <div class="field">
+                  <label class="label">Your Name:
+                    <input class="input" type="text" name="name">
+                  </label>
+                </div>
+
+                <div class="field">
+                  <label class="label">Your Email:
+                    <input class="input" type="email" name="email">
+                  </label>
+                </div>
+                <div class="field">
+                  <div class="control">
+                    <label class="checkbox">
+                      <input type="checkbox"> I would like to recieve updates from Amy Storm Kosman creative &amp; subscribe to the Amy Storm Kosman
+                      creative newsletter.
+                    </label>
+                  </div>
+                </div>
+                <div class="field">
+                  <div class="control">
+                    <label class="checkbox">
+                      <input type="checkbox"> I have read the
+                      <a href="/info/privacy">Privacy Policy</a>. I agree to the terms and understand my rights.
+                    </label>
+                  </div>
+                </div>
+                <p>
+                  <button class="button is-medium is-pink" type="submit">Send</button>
+                </p>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </div>
-    </form>
   </section>
 </template>
+
+
+
 
 <script>
 export default {
@@ -57,26 +79,6 @@ export default {
 .card-footer {
 	border-top: none;
 }
-.success {
-  color: #fefcfb;
-  background-color: #3e9523;
-  border-radius: 5px;
-  padding: 20px 15px;
-  margin-bottom: 30px;
-}
-.close {
-  cursor: pointer;
-  color: #fefcfb;
-  border: 1px solid #fefcfb;
-  width: 24px;
-  height: 24px;
-  float: right;
-  border-radius: 50%;
-  text-align: center;
-  font-weight: 300;
-  line-height: 22px;
-  background-color: transparent;
-}
 .textarea,
 .input {
   border: 2px solid rgb(217, 213, 214);
@@ -86,5 +88,11 @@ export default {
 .is-pink {
   background-color: #ac3b61;
   color: #fefcfb;
+}
+a {
+  color: #171412;
+  text-decoration-line: underline;
+  text-decoration-color: #ac3b61;
+  font-style: italic;
 }
 </style>
