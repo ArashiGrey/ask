6a9b05abf6a79d22667de970a51458fc120ca257
Amy <arashi@tutanota.de>
2018-04-07 21:51:18 +0200

    fix field

--- components/signupNewsletter.vue
+++ components/signupNewsletter.vue
@@ -16,7 +16,6 @@
           <div class="card-content">
             <div class="content">
               <form name="subscribe" @submit.prevent="validateBeforeSubmit" ref="subscribe" netlify-honeypot="bot-field" method="POST" action="/successnews" netlify>
-                <input type="hidden" name="subscribe" value="subscribe" />
                 <p class="is-hidden">
                   <label>Don’t fill this out:
                     <input name="bot-field">
