9376e2a8ccf85e8bca02aa3bde05cc8d546924f3
Amy <arashi@tutanota.de>
2018-04-14 20:16:40 +0200

    hidden

--- components/quickMessage.vue
+++ components/quickMessage.vue
@@ -8,7 +8,7 @@
             <div class="card-content">
               <div class="content">
                 <form name="message" ref="message" netlify-honeypot="bot-field" method="POST" action="successmessage" netlify>
-                  <input type="is-hidden" name="form-name" value="message" />
+                  <input type="hidden" name="form-name" value="message" />
                   <p class="is-hidden">
                     <label>Don’t fill this out:
                       <input name="bot-field">
