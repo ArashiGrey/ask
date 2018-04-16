e9d4e2410c3ce49965fc5bc67c880a6a6bc705c3
Amy <arashi@tutanota.de>
2018-04-16 11:31:15 +0200

    add exclude sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,8 @@ module.exports = {
       { rel: 'icon', type: 'image/png', href: 'favicon-96x96.png', sizes: '96x96' },
       { rel: 'icon', type: 'image/png', href: 'favicon-32x32.png', sizes: '32x32' },
       { rel: 'icon', type: 'image/png', href: 'favicon-16x16.png', sizes: '16x16' },
-      { rel: 'icon', type: 'image/png', href: 'favicon-128.png', sizes: '128x128' }
+      { rel: 'icon', type: 'image/png', href: 'favicon-128.png', sizes: '128x128' },
+      { rel: 'sitemap', href: '/sitemap.xml' }
     ]  
     },
     /*
@@ -64,6 +65,15 @@ module.exports = {
       'nuxt-bulma-slim',
       'nuxtent'
     ],
+    sitemap: {
+      hostname: 'https://askcreative.space',
+      generate: true, // Enable me when using nuxt generate
+      exclude: [
+        '/admin',
+        '/successMessage',
+        '/successNews'
+      ]
+    },
     plugins: [
       // ssr: false to only include it on client-side
       { src: '~/plugins/vee-validate.js' }
