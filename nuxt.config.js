73029ed3085ae20e8a3cc25ef79af0b304dd6b62
Amy <arashi@tutanota.de>
2018-04-23 23:35:40 +0200

    cookies

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,8 +32,10 @@ module.exports = {
       { rel: 'icon', type: 'image/png', href: 'favicon-16x16.png', sizes: '16x16' },
       { rel: 'icon', type: 'image/png', href: 'favicon-128.png', sizes: '128x128' },
       { rel: 'sitemap', href: '/sitemap.xml' }
-    ]  
-    },
+    ],
+    script: [
+      { type: 'text/javascript', id: 'Cookiebot', src: 'https://consent.cookiebot.com/uc.js', datacbid: '7d4f74e8-03e4-4f85-81a7-8c691cd90849',  async: true }  
+    ],
     /*
      ** Global CSS
      */
@@ -86,4 +88,5 @@ module.exports = {
       color: '#ac3b61',
       height: '5px'
     }
+  }
 }
