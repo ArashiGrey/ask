9dd6135e96769acd6fe3e8010bf1cc1de96468fb
Amy <arashi@tutanota.de>
2018-04-28 17:32:11 +0200

    prefetch try

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -4,15 +4,18 @@
     <div class="columns is-centered is-multiline">
       <div class="column">
       </div>
-      <div class="column">
-        <div id='my-store-13580081'></div>  
-        <div v-html="scripts"></div>
+      <div class="column is-one-third-desktop">
+        <div id="my-store-13580081"></div>
+        <div id="productBrowser"></div>
+         <div v-html="scripts"></div>
       </div>
       <div class="column">
       </div>
     </div>
   </section>
 </template>
+
+
 
 <script>
 import shopHero from '~/components/shop/shopHero.vue'
@@ -21,13 +24,11 @@ export default {
   layout: 'defaultLayout',
   scrollToTop: true,
   components: {
-    shopHero
+    shopHero,
     },
-  data: function () {
-    return {
-    scripts: "<div><script type='text/javascript' data-cfasync='false' src='https://app.ecwid.com/script.js?13580081' charset='utf-8'><\/script><script type='text/javascript'> xProductBrowser('categoriesPerRow=3','views=grid(20,3) list(60) table(60)','categoryView=grid','searchView=list','id=my-store-13580081');<\/script></div>"
-    }
-  },
+  data: () => ({
+    scripts: "<script type='text/javascript' data-cfasync='true' src='https://app.ecwid.com/script.js?13580081' charset='utf-8'><\/script><script type='text/javascript'> xProductBrowser('categoriesPerRow=3','views=grid(20,3) list(60) table(60)','categoryView=grid','searchView=list','id=my-store-13580081');<\/script>"
+  }),
   head() {
     return {
       title: 'A.S.K.c Shop',
@@ -35,7 +36,12 @@ export default {
         hid: 'description',
         name: 'description',
         content: 'A place to buy all things Amy Storm Kosman creative.'
-      }]
+      }],
+      link: [
+        { rel:'dns-prefetch', href:'//images-cdn.ecwid.com/' },
+        { rel:'dns-prefetch', href:'//images.ecwid.com/' },
+        { rel:'dns-prefetch', href:'//app.ecwid.com/' }
+      ]
     }
   }
 }
