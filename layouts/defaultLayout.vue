cc34b10c476c1d9ece8a41c0e0f9970242b81a1b
Amy <arashi@tutanota.de>
2018-05-07 17:36:16 +0200

    fix

--- layouts/defaultLayout.vue
+++ layouts/defaultLayout.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <scroll-to/>
     <my-top/>
     <nuxt/>
     <my-bottom/>
@@ -8,13 +7,11 @@
 </template>
 
 <script>
-import scrollTo from '~/components/scrollTo.vue'
 import myTop from '~/components/myTop.vue'
 import myBottom from '~/components/myBottom.vue'
 
 export default {
   components: {
-    scrollTo,
     myTop,
     myBottom
   }
