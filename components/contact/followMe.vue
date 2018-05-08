4767fefb74bbe0c59ddc1a203fcdc099b735b802
Amy <arashi@tutanota.de>
2018-05-08 11:29:43 +0200

    img opty

--- components/contact/followMe.vue
+++ components/contact/followMe.vue
@@ -1,86 +1,62 @@
 <template>
   <section>
-
     <div class="columns is-mobile is-multiline is-centered">
       <div class="column">
         </div>
-        <div class="column">
-       
-           
+        <div class="column"> 
               <a href="https://joindiaspora.com/people/f021cd704b67013555c80242ac110007" target="_blank">
-                <img src="~assets/img/Odiaspora.png" alt="diaspora logo">
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771216/diaspora_p40nlj.png" alt="diaspora logo">
               </a>
-           
           </div>
           <div class="column">
-            
               <a href="https://vk.com/id445845253" target="_blank">
-                <img src="~assets/img/Ovkontakte.png" alt="vkontakte logo">
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771216/vkontakte_tmfz6x.png" alt="vkontakte logo">
               </a>
             </div>
-          
           <div class="column">
-           
               <a href="https://www.facebook.com/askcreative.space" target="_blank">
-                <img src="~assets/img/Ofacebook.png" alt="facebook logo">
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771215/facebook_synael.png" alt="facebook logo">
               </a>
             </div>
-        
           <div class="column">
-          
               <a href="https://twitter.com/askcreative" target="_blank">
-                <img src="~assets/img/Otwitter.png" alt="twitter logo">
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771216/twitter_srdflh.png" alt="twitter logo">
               </a>
-           
           </div>
           <div class="column">
-           
               <a href="https://www.flickr.com/photos/amystormkosman" target="_blank">
-                <img src="~assets/img/Oflickr.png" alt="flickr logo">
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771217/flickr_pvhyvk.png" alt="flickr logo">
               </a>
-            
           </div>
           <div class="column">
-           
               <a href="https://ok.ru/profile/576887571683" target="_blank">
-                <img src="~assets/img/Oodnoklassniki.png" alt="odnoklassniki logo">
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771217/odnoklassniki_znjd9t.png" alt="odnoklassniki logo">
               </a>
-           
           </div>
           <div class="column">
-         
               <a href="https://github.com/askcreative" target="_blank">
-                <img src="~assets/img/Ogithub.png" alt="github logo">
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771217/github_czrubb.png" alt="github logo">
               </a>
-           
           </div>
           <div class="column">
-           
               <a href="https://www.linkedin.com/company/amy-storm-kosman-creative" target="_blank">
-                <img src="~assets/img/Olinkedin.png" alt="linkedin logo">
-              </a>
-          
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771215/linkedin_ztbsae.png" alt="linkedin logo">
+              </a>          
           </div>
-          <div class="column">
-            
+          <div class="column">            
               <a href="https://plus.google.com/103017973685192810679" target="_blank">
-                <img src="~assets/img/Ogoogle-plus.png" alt="google-plus logo">
-              </a>
-            
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771215/google-plus_toa2hh.png" alt="google-plus logo">
+              </a>            
           </div>
-          <div class="column">
-           
+          <div class="column">           
               <a href="https://timeline.line.me/user/_dQeeYLJVMyLECqNeDH9euw80boLtzB-M-2os5p0" target="_blank">
-                <img src="~assets/img/Oline.png" alt="line logo">
-              </a>
-            
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771215/line_wabidq.png" alt="line logo">
+              </a>            
           </div>
-          <div class="column">
-           
+          <div class="column">           
               <a href="https://www.instagram.com/askcreative.space" target="_blank">
-                <img src="~assets/img/Oinstagram.png" alt="instagram logo">
-              </a>
-            
+                <img src="https://res.cloudinary.com/askcreative-space/image/upload/f_auto/v1525771215/instagram_disbz6.png" alt="instagram logo">
+              </a>            
           </div>
               <div class="column">
         </div>
